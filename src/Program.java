2bbd854941ec23fc4a7994787ce19a14d3baa6a8
Baristopcu08 <baristopcu2008@gmail.com>
2022-11-17 00:10:03 +0300

    B2-hW004-OOP End

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,15 @@
 public class Program {
     public static void main(String[] args) {
-        //Lütfen readme.md dosyasını okuyunuz
+
+        House hause1=new House();
+        House hause2=new House(1,2,"Electric");
+
+        hause1.heat();
+        hause1.runCoolingSystem();
+        System.out.println("---------------");
+        hause2.heat();
+        hause2.runCoolingSystem();
+
+
     }
 }
