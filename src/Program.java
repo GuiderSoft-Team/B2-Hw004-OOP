658dc527ff737f0417517a32e6247d948753aa13
reyhanacar <reyhanozkanacar@gmail.com>
2022-11-17 13:32:13 +0300

    Reyhan Acar- OOP homework.

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,24 @@
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+
+        House house1=new House();
+        House house2=new House(house1.getBathroom(), house1.getBedroom(), house1.getHeating());
+
+        house1.setCooling(true);
+
+        house1.heat();
+        house1.runCoolingSystem();
+        System.out.println();
+
+        house2.heat();
+        house2.runCoolingSystem();
+
+
+
+
+
+
+
     }
 }
