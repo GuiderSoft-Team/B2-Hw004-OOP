dd2350a0e546e44a0f00e9514ad62124f6f2a34a
özge topal <the.plus.fineart@gmail.com>
2022-11-17 13:38:35 +0300

    ödev homework4

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,18 @@
+import HouseHomework.House;
+
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+        House house1=new House();
+        House house2=new House(5,6,new String[]{"gas","elektric"});
+
+
+
+        house1.Heat();
+        house1.runCoolingSystem();
+        System.out.println();
+        house2.Heat();
+        house2.runCoolingSystem();
+
     }
 }
