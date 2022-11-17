71fb2a0e274b057b424effba9adf73559813bf49
Sule Komurcu <komurcusulee3@gmail.com>
2022-11-17 19:42:10 +0300

    Şule KÖMÜRCÜ (Homework 4)

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,16 @@
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+        House home=new House();
+        House rent=new House(2, 2, "Yerden ısıtma");
+
+        home.heat();
+        home.runCoolingSystem();
+        System.out.println("-------------");
+        rent.heat();
+        rent.runCoolingSystem();
+
+
+
     }
 }
