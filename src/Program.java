65adb9c2ad893d0c03cd17f5be3d64852ca5d041
krkcm <krkcmstf91@gmail.com>
2022-11-17 03:00:29 +0300

    HW4

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,16 @@
 public class Program {
     public static void main(String[] args) {
-        //Lütfen readme.md dosyasını okuyunuz
+        House house1=new House();
+        house1.runCoolingsystem();
+        house1.heat();
+
+
+        House house2=new House(3,4, "Gas");
+        house2.runCoolingsystem();
+        house2.heat();
+
+
+
+
     }
 }
