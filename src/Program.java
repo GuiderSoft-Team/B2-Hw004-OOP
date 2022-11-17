8f39d5c799617c6661fa853c48861f9d6575e6aa
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-11-17 19:53:48 +0300

    Erkan Cihan YILMAZ has completed. Nov.17.2022

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,12 @@
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+        House h1=new House();
+        House h2=new House(5,3,"Gas");
+        h1.heat();
+        h1.runCoolingSystem();
+        System.out.println("");
+        h2.heat();
+        h2.runCoolingSystem();
     }
 }
