17a65ad492537396507557093d9bc68e069aa483
EnesEvci <enesevci13@gmail.com>
2022-11-17 10:56:52 +0100

    Private members

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,17 @@
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+    House house1 = new House(3,2,"Gas");
+    house1.setCooling(false);
+    House house2 = new House();
+    house2.setBedroom(5);
+    house2.setBathroom(3);
+    house2.setHeating("Electric");
+    house2.setCooling(true);
+
+    house1.heat();
+    house1.runCoolingSystem();
+    house2.heat();
+    house2.runCoolingSystem();
     }
 }
