3642c61e1b127ac412fdc3abc7ea097bf0c690b6
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-18 20:51:16 +0300

    Burcu Uğur - Homework4 is done.

---
+++ src/House.java
@@ -0,0 +1,33 @@
+public class House {
+
+    public int bedroom;
+    public int bathroom;
+    public String heating;
+    public boolean cooling;
+    public boolean hasPool;
+
+    public House() {
+    }
+
+    public House(int bedroom, int bathroom, String heating) {
+        this.bedroom = bedroom;
+        this.bathroom = bathroom;
+        this.heating = heating;
+    }
+
+    public void heat(){
+        System.out.println("The house is heated");
+    }
+
+    public void runCoolingSystem(){
+        if (cooling==true){
+            System.out.println("Cooling system is working");
+        }else {
+            System.out.println("No Cooling System ");
+        }
+
+    }
+
+
+
+}
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,15 @@
 public class Program {
     public static void main(String[] args) {
         //Lütfen readme.md dosyasını okuyunuz
+
+        House h1=new House();
+        House h2=new House();
+
+
+
+        h1.cooling=false;
+        h1.runCoolingSystem();
+        h2.heat();
+
     }
 }
