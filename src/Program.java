0ef2f458eb56b6cd119a825045fa7d8a33978ac8
GİZEM YAYLAGÜL <gizemyaylagul@gmail.com>
2022-11-18 19:20:08 +0300

    Gizem Yaylagül homework 4

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,27 @@
 public class Program {
     public static void main(String[] args) {
-        //Lütfen readme.md dosyasını okuyunuz
+       /* Aşağıdaki özellik ve davranışlara sahip House sınıfı tasarlanacaktır.
+
+        bedroom:int
+        bathroom:int
+        heating:String {Gas, Electric}
+        cooling: boolean
+        hasPool:boolean
+        heat():void output: The house is heated.
+        runCoolingSystem():void {if cooling is true, output: Cooling system is working. if not, output: No Cooling System }
+        create a constructor - no parameter
+        create a constructor - with bedroom, bathroom and heating
+        Program sınıfı - main metodu içerisinde aşağıdaki görevleri yerine getiriniz.
+        create two House objects - one with no parameter constructor and the other with three parameters constructor
+        run heat() and runCoolingSystem() methods*/
+
+        House home=new House();
+        House sweetHome=new House(4,2,"Electric");
+        home.heat();
+        home.runCoolingSystem();
+        System.out.println();
+        sweetHome.heat();
+        sweetHome.runCoolingSystem();
+
     }
 }
