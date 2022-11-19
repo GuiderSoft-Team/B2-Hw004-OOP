845633fed3173aa6929558ba97903e6c750ed1a0
zeynepbozdag <zbozdag208@gmail.com>
2022-11-19 10:56:54 +0100

    Zeynep-Odev yapildi

---
+++ src/House.java
@@ -0,0 +1,29 @@
+public class House {
+
+   private int bedroom;
+   private int bathroom;
+   private String heating; //Gas,Electric;
+   private boolean cooling;
+   private boolean hasPool;
+
+   public House() {
+   }
+
+   public House(int bedroom, int bathroom, String heating) {
+      this.bedroom = bedroom;
+      this.bathroom = bathroom;
+     this.heating=heating;
+   }
+
+   public void heat() {
+      System.out.println("The house is heated.");
+   }
+
+   public void runCoolingSystem() {
+      if (cooling) {
+         System.out.println("Cooling system is working.");
+      } else {
+         System.out.println("No Cooling System");
+      }
+   }
+}
