4f11ca854c5f130d08dad648cd2b6128f2e9ea25
Mümin <mozdamar90@hotmail.com>
2022-11-19 11:35:04 +0300

    Mümin : B2-Hw004-OOP has done.

---
+++ src/House.java
@@ -0,0 +1,32 @@
+public class House {
+    private  int bedroom;
+    private  int bathroom ;
+    private String[] heating={"Gas","Electric"};
+    private Boolean cooling=true;
+    private Boolean hasPool;
+
+    public House() {
+    }
+
+    public House(int bedroom, int bathroom, String[] heating) {
+        this.bedroom = bedroom;
+        this.bathroom = bathroom;
+        this.heating = heating;
+    }
+
+    public void heat(){
+        System.out.println("The house is heated.");
+    }
+    public void runCoolingSystem(){
+        if (cooling){
+            System.out.println("Cooling system is working.");
+
+        }else{
+            System.out.println("No Cooling System ");
+        }
+    }
+
+}
+
+
+
