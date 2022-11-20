3b64b7e66e5d7e8ec21f8de2eb183631b3f7f693
Omer Nalci <oemer.nalci@gmail.com>
2022-11-20 22:35:00 +0100

    Ömer Nalci Hausaufgabe 4 erledigt.

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,14 @@
 public class Program {
     public static void main(String[] args) {
-        //Lütfen readme.md dosyasını okuyunuz
+
+        House h1=new House();
+        House h2=new House(3,1,"Available");
+
+        h1.cooling=true;
+
+        h1.heat();
+        h1.runCoolingSystem();
+
+
     }
 }
