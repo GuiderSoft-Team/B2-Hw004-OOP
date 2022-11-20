482108af1c73827f0628c53a534ca3a3a751a70e
Çağla <mariliith@hotmail.com>
2022-11-20 19:31:45 +0300

    Çağlasu Büke Homework is done. 16/11/2022

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,14 @@
 public class Program {
     public static void main(String[] args) {
-        //Lütfen readme.md dosyasını okuyunuz
+        House house1=new House();
+        House house2=new House(2,2, "Gas");
+
+        house1.heat();
+        house1.runCoolingsystem();
+        System.out.println();
+        house2.heat();
+        house2.runCoolingsystem();
+
+
     }
 }
